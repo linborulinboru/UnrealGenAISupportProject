99bcb6eff1a2ab0fea9a18ac41fe195bf0db8c07
Prajwal Shetty <prajwalshetty3000@gmail.com>
2024-11-18 00:48:24 +0000

    wip setup

--- Source/unreal_llm_api_test/unreal_llm_api_test.Build.cs
+++ Source/unreal_llm_api_test/unreal_llm_api_test.Build.cs
@@ -6,6 +6,7 @@ public class unreal_llm_api_test : ModuleRules
 {
 	public unreal_llm_api_test(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PrivateDependencyModuleNames.AddRange(new string[] { "GenerativeAISupport" });
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
--- Source/unreal_llm_api_test/unreal_llm_api_testGameMode.cpp
+++ Source/unreal_llm_api_test/unreal_llm_api_testGameMode.cpp
@@ -1,7 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "unreal_llm_api_testGameMode.h"
-#include "unreal_llm_api_testCharacter.h"
+
+#include "GenSecureKey.h"
 #include "UObject/ConstructorHelpers.h"
 
 Aunreal_llm_api_testGameMode::Aunreal_llm_api_testGameMode()
@@ -12,4 +13,16 @@ Aunreal_llm_api_testGameMode::Aunreal_llm_api_testGameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+
+	// Get the API key from the plugin
+	FString ApiKey = UGenSecureKey::GetGenerativeAIApiKey();
+	
+	// Display the API key if everything is successful
+	if (GEngine)
+	{
+		FString DisplayMessage = FString::Printf(TEXT("Generative AI API Key: %s"), *ApiKey);
+		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Green, DisplayMessage);
+	}
+
 }
